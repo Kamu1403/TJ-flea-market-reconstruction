04ec9e409fe6225501a5c6be4b17ac12e8fb7b90
UltraQ123 <1355211477@qq.com>
2022-05-08 23:02:04 +0800

    order_temp

--- order/routes.py
+++ order/routes.py
@@ -5,6 +5,7 @@ from order import order_blue
 from app import database
 from flask import render_template, flash, redirect, url_for, request
 from order.models import Order
+from user.models import User_state
 
 
 
@@ -28,15 +29,18 @@ def review(order_id:int):#order_id为订单ID
         get_order = None
     if get_order is None:
         # 报错1
-        pass
+        flash("请求的订单不存在")
+        return redirect(url_for('index'))
     else:
         print(get_order.user_id.id,get_order.op_user_id.id)
         if not current_user.is_authenticated:
-            pass
+            flash("请先登录")
+            return redirect(url_for('user.login'))
             # 报错2
-        elif current_user.id != get_order.user_id.id and current_user.id != get_order.op_user_id:
+        elif current_user.id != get_order.user_id.id and current_user.id != get_order.op_user_id and current_user.state != User_state.Admin.value:
+            flash("您无权访问该订单")
+            return redirect(url_for('user.index'))
             # 报错3
-            pass
         # return redirect(url_for('index'))
     if request.method == "POST":
         pass
@@ -44,7 +48,25 @@ def review(order_id:int):#order_id为订单ID
     return render_template('order_review.html')
 @order_blue.route('/manage', methods=['GET', 'POST'])
 def manage():
+    if not current_user.is_authenticated:
+        flash("请先登录")
+        return redirect(url_for('index'))
+    else:
+        try:
+            print("now:{}".format(current_user.id))
+            user_orders = Order.select().where(Order.user_id_id == current_user.id).execute()
+            order_list = list()
+            for i in user_orders:
+                data = i.__data__
+                data.pop('id')
+                order_list.append(data)
+            # 然后此数据交由前端进行显示
+        except Exception as e:
+            print(repr(e))
     return render_template('order_manage.html')
+
+
 @order_blue.route('/generate', methods=['GET', 'POST'])
 def generate():
+
     return render_template('order_generate.html')
