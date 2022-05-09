4014bb806829d12014e9fadd08f136d4a1c26398
UltraQ123 <1355211477@qq.com>
2022-05-09 17:27:41 +0800

    order_temp

--- order/routes.py
+++ order/routes.py
@@ -4,9 +4,10 @@ from flask_login import current_user
 from order import order_blue
 from app import database
 from flask import render_template, flash, redirect, url_for, request
-from order.models import Order
+from order.models import Order,Contact
+from item.models import Goods,Want
 from user.models import User_state
-
+import json
 
 
 @order_blue.before_request
@@ -58,7 +59,7 @@ def manage():
             order_list = list()
             for i in user_orders:
                 data = i.__data__
-                data.pop('id')
+                #data.pop('id')
                 order_list.append(data)
             # 然后此数据交由前端进行显示
         except Exception as e:
@@ -66,7 +67,34 @@ def manage():
     return render_template('order_manage.html')
 
 
-@order_blue.route('/generate', methods=['GET', 'POST'])
-def generate():
-
+@order_blue.route('/generate/<string:type_name>/<int:item_id>', methods=['GET', 'POST'])
+def generate(type_name:str,item_id:int):
+    if type_name == "goods":
+        bases = Goods
+    elif type_name == "want":
+        bases = Want
+    else:
+        flash("类别错误")
+        return redirect(url_for('index'))
+    if not current_user.is_authenticated:
+        flash("请先登录")
+        return redirect(url_for('index'))
+    try:
+        it = bases.get(bases.id==item_id)
+    except Exception as e:
+        flash("查询失败,请求出错")
+        return redirect(url_for('index'))
+    else:
+        datas = it.__data__
+        print(datas)
+        #op_id = datas['publisher_id']
+        ConData = list()
+        op_ConCats = Contact.select().where(Contact.user_id_id == current_user.id)
+        for i in op_ConCats:
+            data = i.__data__
+            #data.pop('id')
+            ConData.append(data)
+        print(ConData)
     return render_template('order_generate.html')
+
+
