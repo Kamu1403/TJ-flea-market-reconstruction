fafc24049b36d8bc6e3f45735d0f10295993abcd
orbit-clown <643157704@qq.com>
2022-05-10 21:09:42 +0800

    init.py

--- chat/__init__.py
+++ chat/__init__.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-###商品/悬赏管理
-=======
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
->>>>>>> dccade5194ccdcf663b3b14fd61d71b5b131b9a1
+
 from flask import Blueprint
 
 chat_blue = Blueprint('chat', __name__,template_folder="templates", static_folder='static')
