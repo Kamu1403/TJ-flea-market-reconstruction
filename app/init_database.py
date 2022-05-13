9dc5cfe5fa9c1d239b24e5ab85bd0dab39d01fe6
watermellye <didiking@yeah.net>
2022-05-13 23:45:31 +0800

    pic_num

--- app/init_database.py
+++ app/init_database.py
@@ -76,8 +76,15 @@ def fake_data():  #填一些假数据进去
         state=1,  #管理员
         password_hash=generate_password_hash("1951705"),
         email=str(1951705) + "@tongji.edu.cn")
-    User.create(id=1950084, username="陈泓仰", state=1, password_hash=generate_password_hash("1950084"), email=str(1950084) + "@tongji.edu.cn")
-    User.create(id=1951566, username="贾仁军", password_hash=generate_password_hash("1951566"), email=str(1951566) + "@tongji.edu.cn")
+    User.create(id=1950084,
+                username="陈泓仰",
+                state=1,
+                password_hash=generate_password_hash("1950084"),
+                email=str(1950084) + "@tongji.edu.cn")
+    User.create(id=1951566,
+                username="贾仁军",
+                password_hash=generate_password_hash("1951566"),
+                email=str(1951566) + "@tongji.edu.cn")
     User.create(
         id=1953493,
         username="程森",
@@ -87,49 +94,119 @@ def fake_data():  #填一些假数据进去
 
     #反馈
     Feedback.create(user_id=1951705, feedback_content="哈哈哈哈")
-    Feedback.create(user_id=1953493, state=-1, feedback_content="有bug啊", reply_content="bug在哪?")
+    Feedback.create(user_id=1953493,
+                    state=-1,
+                    feedback_content="有bug啊",
+                    reply_content="bug在哪?")
 
     #封号 程森被封到2022.6.1
-    User_Management.create(user_id=1953493, ban_reason="恶意利用网站bug", ban_time=datetime(2022, 6, 1))
+    User_Management.create(user_id=1953493,
+                           ban_reason="恶意利用网站bug",
+                           ban_time=datetime(2022, 6, 1))
 
     #商品、悬赏
-    Item.create(id=1,name="苹果", user_id=1951705, price=1.11, tag="食物", pic_num=2,type=0)
-    Item.create(id=2,name="方便面", user_id=1950084, price=3.33, shelved_num=999,type=0,pic_num=0)
-    Item.create(id=3,name="肉", user_id=1950084, price=10, shelved_num=999,type=0,pic_num=1)
-    Item.create(id=4,name="沈坚作业", user_id=1951705, price=0.01, tag="作业", description="求帮忙写sj作业",type=1)
-    Item.create(id=5,name="耳机", user_id=1953493, price=300, tag="电子用品", description="求耳机一副",type=1,pic_num=2)
+    Item.create(id=1, name="苹果", user_id=1951705, price=1.11, tag="食物", type=0)
+    Item.create(id=2,
+                name="方便面",
+                user_id=1950084,
+                price=3.33,
+                shelved_num=999,
+                type=0)
+    Item.create(id=3,
+                name="肉",
+                user_id=1950084,
+                price=10,
+                shelved_num=999,
+                type=0)
+    Item.create(id=4,
+                name="沈坚作业",
+                user_id=1951705,
+                price=0.01,
+                tag="作业",
+                description="求帮忙写sj作业",
+                type=1)
+    Item.create(id=5,
+                name="耳机",
+                user_id=1953493,
+                price=300,
+                tag="电子用品",
+                description="求耳机一副",
+                type=1)
 
     #浏览 收藏
-    History.create(user_id=1951705, item_id=2,visit_time=datetime(2022,4,1))
+    History.create(user_id=1951705, item_id=2, visit_time=datetime(2022, 4, 1))
     History.create(user_id=1951566, item_id=2)
     History.create(user_id=1950084, item_id=1)
     Favor.create(user_id=1951705, item_id=2)
     Favor.create(user_id=1951566, item_id=3)
     Favor.create(user_id=1950084, item_id=1)
 
-    History.create(user_id=1951705, item_id=5,visit_time=datetime(2022,4,3))
+    History.create(user_id=1951705, item_id=5, visit_time=datetime(2022, 4, 3))
     History.create(user_id=1951566, item_id=5)
     History.create(user_id=1950084, item_id=4)
     Favor.create(user_id=1951705, item_id=5)
     Favor.create(user_id=1951566, item_id=5)
     Favor.create(user_id=1950084, item_id=4)
 
-    Contact.create(id=1,user_id=1951705, name="高曾谊", telephone="+86 111111111", full_address="xxx",default=True)
-    Contact.create(id=2,user_id=1950084, name="陈泓仰", telephone="+86 1112111111", full_address="xyxx",default=True)
-    Contact.create(id=3,user_id=1953493, name="程森", telephone="+86 1111111131", full_address="zzzx",default=True)
-    Contact.create(id=4,user_id=1951566, name="贾仁军", telephone="+86 11123111131", full_address="kkzx",default=True)
+    Contact.create(id=1,
+                   user_id=1951705,
+                   name="高曾谊",
+                   telephone="+86 111111111",
+                   full_address="xxx",
+                   default=True)
+    Contact.create(id=2,
+                   user_id=1950084,
+                   name="陈泓仰",
+                   telephone="+86 1112111111",
+                   full_address="xyxx",
+                   default=True)
+    Contact.create(id=3,
+                   user_id=1953493,
+                   name="程森",
+                   telephone="+86 1111111131",
+                   full_address="zzzx",
+                   default=True)
+    Contact.create(id=4,
+                   user_id=1951566,
+                   name="贾仁军",
+                   telephone="+86 11123111131",
+                   full_address="kkzx",
+                   default=True)
 
-    Review.create(id=1,user_id=1951705, feedback_content="默认好评")
-    Review.create(id=2,user_id=1950084, feedback_content="默认好评")
-    Review.create(id=3,user_id=1953493, feedback_content="默认好评")
-    Review.create(id=4,user_id=1951566, feedback_content="默认好评")
+    Review.create(id=1, user_id=1951705, feedback_content="默认好评")
+    Review.create(id=2, user_id=1950084, feedback_content="默认好评")
+    Review.create(id=3, user_id=1953493, feedback_content="默认好评")
+    Review.create(id=4, user_id=1951566, feedback_content="默认好评")
 
-    Order.create(id=1,user_id=1951566, op_user_id=1951705, contact_id=4, op_contact_id=1, payment=1.11, state=2, end_time=datetime.utcnow())
-    Order.create(id=2,user_id=1953493, op_user_id=1951705, contact_id=3, op_contact_id=1, payment=0.01, state=-1, close_time=datetime.utcnow(), note="我来帮你写sj！")
-    Order.create(id=3,user_id=1951566, op_user_id=1950084, contact_id=4, op_contact_id=2, payment=9.99, state=0)  #3份方便面
+    Order.create(id=1,
+                 user_id=1951566,
+                 op_user_id=1951705,
+                 contact_id=4,
+                 op_contact_id=1,
+                 payment=1.11,
+                 state=2,
+                 end_time=datetime.utcnow())
+    Order.create(id=2,
+                 user_id=1953493,
+                 op_user_id=1951705,
+                 contact_id=3,
+                 op_contact_id=1,
+                 payment=0.01,
+                 state=-1,
+                 close_time=datetime.utcnow(),
+                 note="我来帮你写sj！")
+    Order.create(id=3,
+                 user_id=1951566,
+                 op_user_id=1950084,
+                 contact_id=4,
+                 op_contact_id=2,
+                 payment=9.99,
+                 state=0)  #3份方便面
 
     Order_State_Item.create(order_id=1, user_review_id=1, op_user_review_id=4)
-    Order_State_Item.create(order_id=2, cancel_user=1950084, cancel_reason="沈坚作业不能作为悬赏！")
+    Order_State_Item.create(order_id=2,
+                            cancel_user=1950084,
+                            cancel_reason="沈坚作业不能作为悬赏！")
     Order_State_Item.create(order_id=3)
 
     #订单3中包含 3份方便面
--- item/models.py
+++ item/models.py
@@ -6,19 +6,23 @@ import peewee as pw
 from datetime import datetime
 from user.models import User
 
-from enum import Enum,unique
+from enum import Enum, unique
+
+
 #添加 unique 装饰器
 @unique
 class Item_state(Enum):
     #物品状态 -1-被管理员冻结，无法搜素 0-正常在售(无论上架数量是否为0) 1-商品被上架人自己下架，无法被搜索
     Sale = 0
     Freeze = -1
-    Lock=1
+    Lock = 1
+
+
 @unique
 class Item_type(Enum):
     #物品类型 0-商品 1-悬赏
     Goods = 0
-    Want =1
+    Want = 1
 
 
 class Item(BaseModel):
@@ -35,27 +39,25 @@ class Item(BaseModel):
     publish_time = pw.DateField(verbose_name="发布时间",
                                 null=False,
                                 default=datetime.utcnow())
-    price = pw.FloatField(verbose_name="单价", default=0,null=False)
-    tag=pw.CharField(verbose_name="Tag,用于分类",max_length=128,index=True)
-    state=pw.IntegerField(verbose_name="物品状态",default=Item_state.Sale.value,constraints=[pw.Check("state >=-1")])
-    type=pw.IntegerField(verbose_name="物品类型：悬赏或商品",default=Item_type.Goods.value,constraints=[pw.Check("type>=-1")])
+    price = pw.FloatField(verbose_name="单价", default=0, null=False)
+    tag = pw.CharField(verbose_name="Tag,用于分类", max_length=128, index=True)
+    state = pw.IntegerField(verbose_name="物品状态",
+                            default=Item_state.Sale.value,
+                            constraints=[pw.Check("state >=-1")])
+    type = pw.IntegerField(verbose_name="物品类型：悬赏或商品",
+                           default=Item_type.Goods.value,
+                           constraints=[pw.Check("type>=-1")])
 
     #   普通用户仅能看见上架数量
     #   管理员可看见上架和锁定数量
-    shelved_num = pw.IntegerField(
-        verbose_name="上架数量",
-        null=False,
-        default=1
-    )
-    locked_num = pw.IntegerField(
-        verbose_name="锁定数量",
-        null=False,
-        default=0
-    )
+    shelved_num = pw.IntegerField(verbose_name="上架数量", null=False, default=1)
+    locked_num = pw.IntegerField(verbose_name="锁定数量", null=False, default=0)
 
     description = pw.CharField(verbose_name="详细描述", max_length=1024)
-    pic_num = pw.IntegerField(verbose_name="图片数量", null=False, default=0)
-    #商品缩略图默认存储在 ./item/static/resource/item-pic/{item-id}/{pic_num个文件}
+
+    #pic_num = pw.IntegerField(verbose_name="图片数量", null=False, default=0)
+    #商品缩略图默认存储在 ./item/static/resource/item-pic/{item-id}/缩略图/{pic_num个文件}
+
 
 class History(BaseModel):
     """
@@ -66,6 +68,7 @@ class History(BaseModel):
     user_id = pw.ForeignKeyField(User, verbose_name="访问者的id")
     item_id = pw.ForeignKeyField(Item, verbose_name="item ID")
     visit_time = pw.DateField(verbose_name="访问时间", default=datetime.utcnow())
+
 
 class Favor(BaseModel):
     """
