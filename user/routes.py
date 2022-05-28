56d588c007b1b055e4725f75d20049458769cdde
orbit-clown <643157704@qq.com>
2022-05-28 14:10:31 +0800

    userpublish

--- user/routes.py
+++ user/routes.py
@@ -32,6 +32,10 @@ def order():
     return render_template("user_order.html",current_user=current_user)
 #个人认为，“我的订单”页面不应该被别人看见
 
+@user_blue.route('/publish', methods=['GET', 'POST'])
+def publish():
+    return render_template("user_publish.html",current_user=current_user)
+
 
 #个人中心
 @user_blue.route('/<opt_userid>/space', methods=['GET', 'POST'])
