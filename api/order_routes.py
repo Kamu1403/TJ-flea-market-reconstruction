016d1dd72c9e357d23bb9d4e147044cece93e23f
gzy02 <1262454489@qq.com>
2022-05-29 21:38:56 +0800

    order bug check

--- api/order_routes.py
+++ api/order_routes.py
@@ -47,7 +47,7 @@ def change_order_status():
     elif current_user.id != order_user_id and current_user.id != order_op_user_id:  #不是订单双方
         return make_response_json(401, "当前用户不是订单双方之一，未授权")
     elif req_state == order.state:  #要修改的状态和数据库内订单状态重复了，改不改都一样，直接返回成功
-        return make_response_json(200, "操作成功")
+        return make_response_json(400, "操作过于频繁")
     elif order.state == Order_state.End.value or order.state == Order_state.Close.value:  #订单处于完成或关闭的状态
         return make_response_json(401, "当前订单已完成或取消，操作失败，当前用户未授权")
 
